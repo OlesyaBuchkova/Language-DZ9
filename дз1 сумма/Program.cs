0a77b4fbd7a05c5cb86bbaade06fc6de20719e26
olesya <nearolesya@gmil.com>
2023-09-17 21:03:10 +0300

    ДЗ к семинару 9

---
+++ дз1 сумма/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Print(string text) 
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int SumNumber (int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    return N + SumNumber(M, N - 1);
+}
+
+int M = Print("Введите число M: ");
+int N = Print("Введите число N: ");
+Console.WriteLine(SumNumber(M, N));
---
+++ дз2 Аккерман/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Print(string text) 
+{
+    Console.WriteLine(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+} 
+int Akkerman(int M, int N)
+{
+    if (M == 0) 
+    {
+        return N + 1;
+    }
+    else if (M > 0 && N == 0) 
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else 
+    {
+    return Akkerman(M - 1, Akkerman(M, N - 1));
+    }
+}
+int M = Print("Введите неотрицательное число M: ");
+int N = Print("Введите неотрицательное число N: ");
+Console.Write($"Функция Аккермана равно {Akkerman(M, N)} ");
